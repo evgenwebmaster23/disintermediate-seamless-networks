81a6bd7e0748c5ce190e50da6debc0988329f3b9
evgenwebmaster23 <161200325+evgenwebmaster23@users.noreply.github.com>
2024-03-25 11:11:34 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,41 @@
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        }
+    }
+
+        int[] data = new int[10];
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+
+        }    }
+        for (int item : data) {
+
+        int[] data = new int[10];
+    public static void main(String[] args) {
+
+            System.out.println("Random Number: " + item);
+        }
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+}
+function main() {
+
+    return data;
+}
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+
+    }
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+function generateRandomData() {
+    data.forEach(item => {
+    });
+}
+    for (let i = 0; i < 10; i++) {
